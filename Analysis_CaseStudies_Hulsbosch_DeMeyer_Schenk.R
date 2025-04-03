cd32f98e80f1b1d74cee9948dc7b0c7c755f3305
murolll <merel.schenk@ugent.be>
2025-04-03 13:15:08 +0200

    Add files via upload

---
+++ Analysis_CaseStudies_Hulsbosch_DeMeyer_Schenk.R
@@ -0,0 +1,5 @@
+##Case studies in the Analysis of Experimental Psychology##
+##Group: Judith de Meyer, Tristan Hulsbosch, Merel Schenk##
+##Speed dating dataset##
+
+##Reading in the file and filter the data####
